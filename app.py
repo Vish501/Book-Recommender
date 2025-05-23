bc7be4c5b55f5e043014aabf462d32151fe16000
Vish501 <76504332+Vish501@users.noreply.github.com>
2025-05-24 01:29:34 +0530

    moving functions to src/helper.py

--- app.py
+++ app.py
@@ -4,16 +4,13 @@ import os
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 
 import pandas as pd
-import numpy as np
-
 import gradio as gr
 
 from dotenv import load_dotenv
-from functools import partial
 from langchain.schema import Document
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
+from src.helper import recommend_books
 
 # Loading environment variables from .env file
 load_dotenv()
@@ -43,42 +40,6 @@ embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001" )
 
 # Constructing the database
 db_books = Chroma.from_documents(document_object, embedding=embeddings)
-
-# Function to retrieve all the recommendations as a dataframe
-# Inputs - Dataset, Chroma vector database, query, number of recommandations
-# Outputs - List of Books as a df
-def retrieve_semantic_recommendations(books: pd.DataFrame, db_books: Chroma, query: str, no_of_recc: int) -> pd.DataFrame:
-    response_books = db_books.similarity_search(query, k=no_of_recc)
-    isbn_list = [int(doc_object.metadata["source"]) for doc_object in response_books]
-    return books[books["isbn13"].isin(isbn_list)].head(no_of_recc)
-
-# Function to filter books based on category and tone
-def filter_books(books: pd.DataFrame, category: str, tone: str, final_no_of_recc: int) -> pd.DataFrame:
-    if category != "All":
-        books = books[books["base_categories"] == category]
-
-    books = books.head(final_no_of_recc)
-    if tone != "All":
-            books.sort_values(by=tone.lower(), ascending=False, inplace=True)
-
-    return books
-
-# Function to recommend books based on semantic similarity and optional filters
-# Inputs - Dataset, Chroma vector database, query, category, tone, name, initial top_k, final top_k
-# Outputs - List of (thumbnail, caption) tuples to display in the UI
-def recommend_books(books: pd.DataFrame, db_books: Chroma, query: str, category: str, tone: str,
-                    inital_no_of_recc: int = 50, final_no_of_recc: int = 24):
-
-    recommendations = retrieve_semantic_recommendations(books, db_books, query, inital_no_of_recc)
-    filtered_recommendations = filter_books(recommendations, category, tone, final_no_of_recc)
-    print(filtered_recommendations) ### Terminal Output
-
-    # Create a list of (thumbnail, caption) pairs for display
-    results = []
-    for _, row in filtered_recommendations.iterrows():
-        caption = f"{row['title']} by {row['authors']}: {row['description']}"
-        results.append((row["large_thumbnail"], caption))
-    return results
 
 # Intermediate function for `recommend_books` due to issues with Gardio
 def recommend_books_inter(query: str, category: str, tone: str):
